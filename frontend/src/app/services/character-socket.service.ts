f52ee8814331839d7e510b5c63dba741ab06ee18
adermake <adermake@web.de>
2025-12-17 21:08:15 +0100

    x

--- frontend/src/app/services/character-socket.service.ts
+++ frontend/src/app/services/character-socket.service.ts
@@ -3,7 +3,6 @@ import { Subject } from 'rxjs';
 import { io, Socket } from 'socket.io-client';
 import { JsonPatch } from '../model/json-patch.model';
 
-
 @Injectable({ providedIn: 'root' })
 export class CharacterSocketService {
   private socket?: Socket;
@@ -13,7 +12,7 @@ export class CharacterSocketService {
 
   connect() {
     if (this.socket) return;
-
+    console.log('Connecting to socket!');
     this.socket = io(window.location.origin, {
       path: '/socket.io',
       transports: ['websocket'],
@@ -25,10 +24,12 @@ export class CharacterSocketService {
   }
 
   joinCharacter(characterId: string) {
+    console.log('Joining character!');
     this.socket?.emit('joinCharacter', characterId);
   }
 
   sendPatch(characterId: string, patch: JsonPatch) {
+    console.log('Sending patch');
     this.socket?.emit('patchCharacter', { characterId, patch });
   }
 }
