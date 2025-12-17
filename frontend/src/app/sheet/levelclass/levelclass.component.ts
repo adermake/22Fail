7410f38dd124fc016e1889a2501f874b877d3ce0
adermake <adermake@web.de>
2025-12-17 23:06:05 +0100

    br

--- frontend/src/app/sheet/levelclass/levelclass.component.ts
+++ frontend/src/app/sheet/levelclass/levelclass.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input } from '@angular/core';
-import { CardComponent } from "../../shared/card/card.component";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CardComponent } from '../../shared/card/card.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CharacterSheet } from '../../model/character-sheet-model';
+import { JsonPatch } from '../../model/json-patch.model';
 
 @Component({
   selector: 'app-levelclass',
-  imports: [CardComponent,CommonModule,FormsModule],
+  imports: [CardComponent, CommonModule, FormsModule],
   templateUrl: './levelclass.component.html',
   styleUrl: './levelclass.component.css',
 })
 export class LevelclassComponent {
-  @Input({required:true}) sheet!: CharacterSheet;
+  @Input({ required: true }) sheet!: CharacterSheet;
+
+  @Output() patch = new EventEmitter<JsonPatch>();
+
+  updateField(path: string, value: any) {
+    this.patch.emit({ path, value });
+  }
 }
