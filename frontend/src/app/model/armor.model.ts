e32db37e15807af757276818abaef69d8d16bc94
adermake <adermake@web.de>
2026-02-25 22:32:24 +0100

    x

--- frontend/src/app/model/armor.model.ts
+++ frontend/src/app/model/armor.model.ts
@@ -1,17 +1,9 @@
 import { ItemBlock, ItemRequirements } from './item-block.model';
 import { Material } from './weapon.model';
 
-export enum ArmorType {
-    HELMET = 'Helm',
-    CHEST = 'Brustpanzer',
-    LEGGINGS = 'Beinschutz',
-    BOOTS = 'Stiefel',
-    GAUNTLETS = 'Handschuhe',
-}
-
 export class Armor extends ItemBlock {
   // durability and stability are now inherited from ItemBlock
-  armorType!: ArmorType;
+  // armorType is also inherited from ItemBlock
 
   // For crafting
   primaryMaterial!: Material;
